fcb1ae10522705f94a200a8ee4e8a8ac15b83528
Zahra Abbasi <zahra.abbasi.m01@gmail.com>
2024-10-20 21:46:38 +0330

    set up up growth

---
+++ set_up_gpu_growth.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+gpu_id = 0
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        tf.config.experimental.set_visible_devices(gpus[gpu_id], 'GPU')
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
